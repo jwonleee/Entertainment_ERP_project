e0c471e5b8756f352a24c5c3f3f1f5a1bf0ff4b8
dsdudwkd <dsdudwkd@naver.com>
2023-03-10 12:33:57 +0900

    EY

--- src/main/java/com/y4j/final_project/user/service/UserMapper.java
+++ src/main/java/com/y4j/final_project/user/service/UserMapper.java
@@ -1,10 +1,13 @@
 package com.y4j.final_project.user.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.validation.Errors;
 
 import com.y4j.final_project.command.UserVO;
+import com.y4j.final_project.util.Criteria;
 
 
 @Mapper
@@ -14,5 +17,8 @@ public interface UserMapper {
 	public int registUser(UserVO vo);
 	
 	//유저 정보 조회 메서드
-	public ArrayList<UserVO> getUserList();
+	public ArrayList<UserVO> getUserList(Criteria cri);
+	
+	//전체 데이터 수 반환 메서드
+	public int getUserTotal(Criteria cri);
 }
--- src/main/java/com/y4j/final_project/user/service/UserServiceImpl.java
+++ src/main/java/com/y4j/final_project/user/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.y4j.final_project.user.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
 import com.y4j.final_project.command.UserVO;
+import com.y4j.final_project.util.Criteria;
 
 
 @Service @Primary
@@ -18,14 +24,21 @@ public class UserServiceImpl implements UserService {
 	
 	//유저 등록 메서드
 	public int registUser(UserVO vo) {
+
 		return userMapper.registUser(vo);
 	}
 	
 	//유저 정보 조회 메서드
-	public ArrayList<UserVO> getUserList() {
-		return userMapper.getUserList();
+	@Override
+	public ArrayList<UserVO> getUserList(Criteria cri) {
+		return userMapper.getUserList(cri);
 	}
-	
-	
+
+	@Override
+	public int getUserTotal(Criteria cri) {
+
+		return userMapper.getUserTotal(cri);
+	}
+
 	
 }
