1352a287f9c48c5c5a5735e97fec3b343186046d
hormonee <mmmobl@naver.com>
2023-03-27 19:33:33 +0900

    230327 19:33 jm

--- src/main/java/com/y4j/final_project/controller/UserController.java
+++ src/main/java/com/y4j/final_project/controller/UserController.java
@@ -1,43 +1,32 @@
 package com.y4j.final_project.controller;
 
-import javax.servlet.http.Cookie;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.y4j.final_project.command.CartVO;
 import com.y4j.final_project.command.AlbumCartVO;
 import com.y4j.final_project.command.CartListVO1;
 import com.y4j.final_project.command.UserVO;
-import com.y4j.final_project.email.service.EmailService;
 import com.y4j.final_project.user.service.UserService;
-import com.y4j.final_project.util.Criteria;
 import com.y4j.final_project.util.GoogleAPI;
 import com.y4j.final_project.util.KakaoAPI;
 import com.y4j.final_project.util.NaverAPI;
-
-import java.util.*;
 
 
 @Controller
@@ -50,8 +39,8 @@ public class UserController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@Autowired
-	private EmailService emailService;
+//	@Autowired
+//	private EmailService emailService;
 
 	@Autowired
 	private KakaoAPI kakao;
--- src/main/java/com/y4j/final_project/email/service/EmailService.java
+++ src/main/java/com/y4j/final_project/email/service/EmailService.java
@@ -1,5 +1,5 @@
-package com.y4j.final_project.email.service;
-
-public interface EmailService {
-	 String sendSimpleMessage(String to)throws Exception;
-}
+//package com.y4j.final_project.email.service;
+//
+//public interface EmailService {
+//	 String sendSimpleMessage(String to)throws Exception;
+//}
